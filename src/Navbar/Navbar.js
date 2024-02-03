f57b2e78eecb2aa4464cacab0deb12b1e731f942
Harsh-deepsingh <harshdeep.singh01@edu.sait.ca>
2024-02-03 13:18:35 -0700

    incresed tokens

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,7 +6,9 @@ const Navbar = () => {
     <div className="navbar">
       <div className="leftAlign">
       <div className="profile">
+        <a href="https://www.linkedin.com/in/harshdeepsingh9828/" target="blank">
         <img src="LinkedIn.jpg" alt="LinkedIn"></img>
+        </a>
       </div>
       </div>
       IRONMAN AI
